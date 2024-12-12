22600fac5de30bd87d14fb2897201ef3c4a1f935
Geekirui120 <geekirui120@gmail.com>
2024-12-12 14:49:40 +0300

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+document.addEventListener('DOMContentLoaded', () => {
+    // Select the element whose text needs to be updated
+    const targetElement = document.getElementById('text'); // Ensure the element has the correct ID
+    
+    // Update its text content
+    targetElement.textContent = 'This is really cool!';
+  });
+  
